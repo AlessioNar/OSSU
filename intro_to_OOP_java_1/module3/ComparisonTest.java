2e92a83044c223e3a74791a87ecc08f5160b61bc
alessio.nardin@gmail.com <alessio.nardin@gmail.com>
2021-03-03 16:40:45 +0100

    Progress module 3

---
+++ intro_to_OOP_java_1/module3/ComparisonTest.java
@@ -0,0 +1,13 @@
+public class ComparisonTest
+{
+  public static void main(String[] args)
+  {
+    String x = "park";
+    String y = " p4rk";
+
+    if (x.compareTo(y) > 0)
+    {
+      System.out.println("x is greater than y");
+    }
+  }
+}
--- intro_to_OOP_java_1/module3/FahrenheitToCelsius.java
+++ intro_to_OOP_java_1/module3/FahrenheitToCelsius.java
@@ -12,8 +12,16 @@ public class FahrenheitToCelsius {
     double celsius = (5.0/9) * (fahrenheit - 32);
     System.out.printf("%s Fahrenheit: %d\n", day, fahrenheit);
     System.out.printf("%s %-10s: %.2f\n", day, cText, celsius);
+
     if (fahrenheit >= 70)
-      System.out.println("Yay! Go to the park.");
-    
+    {
+      System.out.println("Yay! Go to park.");
+    }
+    else
+    {
+      System.out.println("Stay home where there’s wi-fi and learn 1331 online");
+    }
+
+
   }
 }
