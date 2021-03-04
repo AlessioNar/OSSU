15d0d4647c23c829729881b3d386fbf01b710f0e
alessio.nardin@gmail.com <alessio.nardin@gmail.com>
2021-03-04 21:03:38 +0100

    Exercise almost completed. Still issues with validating user inputs and exiting nicely

--- intro_to_OOP_java_1/module3/Calculator.java
+++ intro_to_OOP_java_1/module3/Calculator.java
@@ -2,45 +2,6 @@ import java.util.Scanner;
 
 public class Calculator
 {
-  public static int integers(String operation, Scanner input)
-  {
-    int a, b;
-    System.out.print("Enter two integers: \n");
-    a = input.nextInt();
-    b = input.nextInt();
-    int result = 0;
-    switch(operation)
-    {
-      case "add":
-        result = a + b;
-        break;
-      case "substract":
-        result = a - b;
-        break;
-    }
-    return result;
-  }
-
-  public static double doubles(String operation, Scanner input)
-  {
-    double a, b;
-    System.out.print("Enter two doubles: \n");
-    a = input.nextDouble();
-    b = input.nextDouble();
-    double result = 0;
-    switch(operation)
-    {
-      case "multiply":
-        result = a * b;
-        break;
-      case "divide":
-        result = a / b;
-        break;
-    }
-    return result;
-  }
-
-
   public static void main(String[] args)
   {
 
@@ -54,35 +15,115 @@ public class Calculator
     switch(operation)
     {
       case "add":
-        int added = integers(operation, input);
-        System.out.printf("Answer: %d\n", added);
-
+      {
+        int a, b;
+        System.out.print("Enter two integers: \n");
+        a = input.nextInt();
+        b = input.nextInt();
+        if (a == (int) a & b == (int) b)
+        {
+          int result = a + b;
+          System.out.printf("Answer: %d\n", result);
+          break;
+        }
+        else
+        {
+          System.out.printf("Invalid input entered. Terminating...\n");
+          break;
+        }
+      }
       case "subtract":
-        // code block
-        int subtracted = integers(operation, input);
-        System.out.printf("Answer: %d\n", subtracted);
-
+      {
+        int a, b;
+        System.out.print("Enter two integers: \n");
+        a = input.nextInt();
+        b = input.nextInt();
+        if (a == (int) a & b == (int) b)
+        {
+          int result = a - b;
+          System.out.printf("Answer: %d\n", result);
+          break;
+        }
+        else
+        {
+          System.out.printf("Invalid input entered. Terminating...\n");
+          break;
+        }
+      }
       case "multiply":
-          // code block
-          double multiplied = doubles(operation, input);
-          System.out.printf("Answer: %d\n", multiplied);
-
+      {
+          double a, b;
+          System.out.print("Enter two doubles: \n");
+          a = input.nextDouble();
+          b = input.nextDouble();
+          if (a == (double) a & b == (double) b)
+          {
+            double result = a * b;
+            System.out.printf("Answer: %.2f\n", result);
+            break;
+          }
+          else
+          {
+            System.out.printf("Invalid input entered. Terminating...\n");
+            break;
+          }
+      }
       case "divide":
-            // code block
-            double divided = doubles(operation, input);
-            System.out.printf("Answer: %d\n", divided);
+      {
+        double a, b;
+        System.out.print("Enter two doubles: \n");
+        a = input.nextDouble();
+        b = input.nextDouble();
 
-        break;
+        if (a == (double) a & b == (double) b)
+        {
+          if (b == 0)
+          {
+            System.out.print("Invalid input entered. Terminating... \n");
+          }
+          else
+          {
+            double result = a / b;
+            System.out.printf("Answer: %.2f\n", result);
+          }
+          break;
+        }
+        else
+        {
+          System.out.printf("Invalid input entered. Terminating...\n");
+          break;
+        }
+      }
       case "alphabetize":
-            // code block
-        break;
+      {
+        String a, b;
+        System.out.print("Enter two words: \n");
+        a = input.next();
+        b = input.next();
+        char a_init = a.charAt(0);
+        char b_init = b.charAt(0);
 
+        a_init = Character.toLowerCase(a_init);
+        b_init = Character.toLowerCase(b_init);
+
+        if (a_init < b_init)
+        {
+          System.out.printf("Answer: %s comes before %s alphabetically.\n", a, b);
+        }
+        else if(b_init < a_init)
+        {
+          System.out.printf("Answer: %s comes before %s alphabetically.\n", b, a);
+        }
+        else
+        {
+          System.out.printf("Answer: Chicken or Egg.\n");
+        }
+        break;
+      }
       default:
         System.out.printf("Invalid input entered. Terminating...\n");
         break;
-    // code block
+
     }
   }
-
-
 }
