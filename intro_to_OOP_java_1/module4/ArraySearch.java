ccdee8640b3532ba83a3d2c485c6999d6ebb1d11
alessio.nardin@gmail.com <alessio.nardin@gmail.com>
2021-03-05 13:37:56 +0100

    method lesson

---
+++ intro_to_OOP_java_1/module4/ArraySearch.java
@@ -0,0 +1,27 @@
+public class ArraySearch
+{
+  public static void main(String args[])
+  {
+    String[] concepts = {"abstraction", "polymorphism",
+                        "inheritance", "encapsulation"};
+
+    System.out.println(searchStringArray("polymorphism", concepts));
+    System.out.println(searchStringArray("abstraction", concepts));
+
+  }
+
+  public static boolean searchStringArray(String target, String[] array)
+  {
+    boolean result = false;
+    for (String element : array)
+    {
+      if (element.equals(target))
+      {
+        result = true;
+        break;
+      }
+    }
+    return result;
+  }
+
+}
